b0c62bf30aa1b733a72f52091e0ab56edfe366c7
Niloy Sikdar <niloysikdar30@gmail.com>
2024-03-18 18:48:56 +0530

    chore(db): update drizzle config and rename migrations folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  schema: './src/schema/*',
-  out: './drizzle',
-  driver: 'pg',
+  schema: "./src/schema/*",
+  out: "./migrations",
+  driver: "pg",
   dbCredentials: {
     connectionString: process.env.POSTGRES_URL!,
   },
