31997e69a0fffe2dc5ba2d07b8469384eacce517
Niloy Sikdar <niloysikdar30@gmail.com>
2024-03-18 17:03:15 +0530

    feat(dashboard): user profile in sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,25 +1,32 @@
 import { Separator } from "@/components/ui/separator";
 import { SIDEBAR_ITEMS } from "./data";
 import { SidebarEachLink } from "./each-link";
+import { UserProfile } from "./user-profile";
 
 export function Sidebar() {
   return (
-    <div className="group flex flex-col w-64 border-r min-h-dvh fixed top-0 left-0 bg-background z-50">
-      <div className="flex items-center px-4 py-4 pl-6">
-        <p className="text-xl font-bold">Emailer</p>
+    <div className="group flex flex-col justify-between w-64 border-r min-h-dvh fixed top-0 left-0 bg-background z-50">
+      <div>
+        <div className="flex items-center px-4 py-4 pl-6">
+          <p className="text-xl font-bold">Emailer</p>
+        </div>
+
+        <Separator />
+
+        <nav className="grid gap-2 p-2">
+          {SIDEBAR_ITEMS.map((item, index) => (
+            <SidebarEachLink
+              key={item.link}
+              index={index}
+              item={{ title: item.title, link: item.link }}
+            />
+          ))}
+        </nav>
       </div>
 
-      <Separator />
-
-      <nav className="grid gap-2 p-2">
-        {SIDEBAR_ITEMS.map((item, index) => (
-          <SidebarEachLink
-            key={item.link}
-            index={index}
-            item={{ title: item.title, link: item.link }}
-          />
-        ))}
-      </nav>
+      <div className="pb-4">
+        <UserProfile />
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/user-profile.tsx
@@ -0,0 +1,130 @@
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  MoreVertical,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { auth } from "@/lib/auth";
+
+export async function UserProfile() {
+  const { session } = await auth();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="flex cursor-pointer items-center justify-between gap-x-3 rounded-md border bg-gray-100 py-1 px-2 text-sm font-semibold leading-6 text-gray-800 hover:bg-gray-200 hover:text-gray-900 mx-3">
+          <div className="flex items-center">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full text-sx">
+              {session?.user?.name?.charAt(0)}
+              {session?.user?.name &&
+                session?.user?.name?.split(" ").length > 1 &&
+                session?.user?.name?.split(" ")[1].charAt(0)}
+            </div>
+            <div className="ml-3">
+              <p className="truncate font-medium text-sm">
+                {session?.user?.name}
+              </p>
+              <p className="truncate font-normal text-xs">
+                {session?.user?.email}
+              </p>
+            </div>
+          </div>
+          <MoreVertical className="h-5 w-5 shrink-0" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Keyboard className="mr-2 h-4 w-4" />
+            <span>Keyboard shortcuts</span>
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <UserPlus className="mr-2 h-4 w-4" />
+              <span>Invite users</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>
+                  <Mail className="mr-2 h-4 w-4" />
+                  <span>Email</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <MessageSquare className="mr-2 h-4 w-4" />
+                  <span>Message</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <PlusCircle className="mr-2 h-4 w-4" />
+                  <span>More...</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            <Plus className="mr-2 h-4 w-4" />
+            <span>New Team</span>
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
